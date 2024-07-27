b26076291182c9a2869bf47e0bf96c85f7acfcac
Artem Petrosyan <artem.petrosyan@magenta-technology.com>
2024-07-27 10:07:13 +0200

    task-1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Мир!");
+        System.out.println("Привет, Мир!!!!!!!!!!!!");
         var a = 2;
         if (a >= 0) {
             System.out.println("Число положительное");
